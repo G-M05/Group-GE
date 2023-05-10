e66752483b58d6b624c31e52e0de52d9ca035777
G-M05 <gregoriomarin2005@gmail.com>
2023-05-10 13:14:17 -0700

    no

--- cave rock jumper/Assets/Greg/Scripts/bulletDestroy.cs
+++ cave rock jumper/Assets/Greg/Scripts/bulletDestroy.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class bulletDestroy : MonoBehaviour
 {
-    
+
     //checks to see if bullet colides with something if it does the bullet gets destroyed
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+
         // Enemies to tak damage
         if (collision.gameObject.TryGetComponent<EnemyAi>(out EnemyAi enemyComponent))
         {
@@ -16,18 +16,17 @@ public class bulletDestroy : MonoBehaviour
             enemyComponent.TakeDamage(1);
 
         }
-<<<<<<< Updated upstream
-        Destroy(gameObject); //destry bullet in all cases
-=======
+
+
+
         else if (collision.gameObject.TryGetComponent<BossBehavior>(out BossBehavior bossComponent))
         {
             bossComponent.TakeDamage(1);
         }
 
-        
-            Destroy(gameObject); //destry bullet in all cases
-        
->>>>>>> Stashed changes
+
+        Destroy(gameObject); //destry bullet in all cases
+
+
     }
-    
 }
