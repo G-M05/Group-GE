46d1e1ca98d8e700a5e4532ef8a73033e56c739c
EliLugo71 <128425669+EliLugo71@users.noreply.github.com>
2023-05-16 14:02:22 -0700

    missed

--- cave rock jumper/Assets/SceneLoader.cs
+++ cave rock jumper/Assets/SceneLoader.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool IsInteracting = false;
+    private void Start()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            IsInteracting = true;
+        }
+        else {
+            IsInteracting = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (IsInteracting) {
+            LoadNextScene();
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        int currectSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currectSceneIndex + 1;
+
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.Log("No more scenes to load.");
+        }
     }
 }
